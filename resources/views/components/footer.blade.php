03d443e8b0d82e91745f5e8b07b281bd4903c9fc
gergelyn <preszthegas@gmail.com>
2021-10-20 13:40:19 +0200

    links added to footer navigation

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,27 +2,27 @@
     <nav class="flex flex-col">
         <ul class="flex items-center justify-center pt-8 pb-4">
             <li>
-                <a href="#" class="p-3">Impresszum</a>
+                <a href="/impresszum" class="p-3">Impresszum</a>
             </li>
             <li>
-                <a href="#" class="p-3">Felhasználási feltételek</a>
+                <a href="/felhasznalasi-feltetelek" class="p-3">Felhasználási feltételek</a>
             </li>
             <li>
-                <a href="#" class="p-3">Adatvédelem</a>
+                <a href="/adatvedelem" class="p-3">Adatvédelem</a>
             </li>
             <li>
-                <a href="#" class="p-3">Süti beállítások</a>
+                <a href="/suti-beallitasok" class="p-3">Süti beállítások</a>
             </li>
         </ul>
         <ul class="flex items-center justify-center p-4">
             <li>
-                <a href="#" class="p-3"><i class="fa-brands fa-youtube"></i></a>
+                <a href="https://youtube.com" class="p-3"><i class="fa-brands fa-youtube"></i></a>
             </li>
             <li>
-                <a href="#" class="p-3"><i class="fa-brands fa-tiktok"></i></a>
+                <a href="https://tiktok.com" class="p-3"><i class="fa-brands fa-tiktok"></i></a>
             </li>
             <li>
-                <a href="#" class="p-3"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://instagram.com" class="p-3"><i class="fa-brands fa-instagram"></i></a>
             </li>
         </ul>
         <p class="text-center pt-4 pb-8">&copy; Minden jog fenntartva</p>
