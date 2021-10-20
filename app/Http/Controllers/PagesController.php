e0d81b6a15025eb84efd5ab109127564bdfa7435
gergelyn <preszthegas@gmail.com>
2021-10-20 12:32:08 +0200

    PagesController created

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function impresszum() {
+        $title = "Best-Recipes | Impresszum";
+        return view('pages.impresszum')->with('title', $title);
+    }
+
+    public function adatvedelem() {
+        $title = "Best-Recipes | Adatvédelem";
+        return view('pages.adatvedelem')->with('title', $title);
+    }
+
+    public function suti() {
+        $title = "Best-Recipes | Süti beállítások";
+        return view('pages.suti');
+    }
+
+    public function felhasznalasi_feltetelek() {
+        $title = "Best-Recipes | Felhasználási feltételek";
+        return view('pages.felhasznalasif')->with('title', $title);
+    }
+}
