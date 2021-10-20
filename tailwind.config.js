17af492f8c38cddf8d31338aefbf668f520f9b69
gergelyn <preszthegas@gmail.com>
2021-10-20 08:27:53 +0200

    tailwind.config.js created with npx and added fonts

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'lobster': ['Lobster', 'cursive'],
+      'poppins': ['Poppins', 'sans-serif'],
+      'playfair': ['Playfair Display', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
