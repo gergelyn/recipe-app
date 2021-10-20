8f9580e3f0c4a24a5228304006a03c6cbcdfad78
gergelyn <preszthegas@gmail.com>
2021-10-20 08:28:51 +0200

    Navbar coded

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="bg-white flex justify-between p-6 mx-48">
+        <ul class="flex items-center">
+            <li>
+                <a href="#" class="p-3 pr-6 font-lobster text-2xl text-yellow-600">Best-Recipes</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Receptek</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Blog</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Vlog</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li class="mr-6">
+                <a href="#" class="p-3 border border-yellow-600 rounded-full text-yellow-600">Belépés</a>
+            </li>
+            <li>
+                <a href="#" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white">Regisztráció</a>
+            </li>
+        </ul>
+    </nav>
+</header>
