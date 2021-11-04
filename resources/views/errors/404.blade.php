fad0636c6508c34d7f118ab68f753d510cb632ea
gergelyn <preszthegas@gmail.com>
2021-11-04 15:04:57 +0100

    404 handling with image added

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <main class="mx-56 py-10">
+        <img src="{{ asset('images/404.svg') }}" alt="404-page-not-found-image">
+    </main>
+@endsection
