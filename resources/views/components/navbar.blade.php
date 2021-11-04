87799c5494c4b76d9fa4e06d145969e4055161c7
gergelyn <preszthegas@gmail.com>
2021-11-04 15:06:31 +0100

    Logo's link changed to /

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,7 +2,7 @@
     <nav class="bg-white flex justify-between p-6 mx-48">
         <ul class="flex items-center">
             <li>
-                <a href="#" class="p-3 pr-6 font-lobster text-2xl text-yellow-600">Best-Recipes</a>
+                <a href="/" class="p-3 pr-6 font-lobster text-2xl text-yellow-600 hover:text-yellow-700">Best-Recipes</a>
             </li>
             <li>
                 <a href="#" class="p-3">Receptek</a>
@@ -16,10 +16,10 @@
         </ul>
         <ul class="flex items-center">
             <li class="mr-6">
-                <a href="#" class="p-3 border border-yellow-600 rounded-full text-yellow-600">Belépés</a>
+                <a href="#" class="p-3 border border-yellow-600 hover:border-yellow-700 hover:bg-yellow-700 hover:text-white rounded-full text-yellow-600">Belépés</a>
             </li>
             <li>
-                <a href="#" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white">Regisztráció</a>
+                <a href="#" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700">Regisztráció</a>
             </li>
         </ul>
     </nav>
