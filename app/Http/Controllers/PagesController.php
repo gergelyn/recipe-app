bc9839890e337cfb65bc269076a703196124dbc5
gergelyn <preszthegas@gmail.com>
2021-11-04 15:03:10 +0100

    Title change

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,22 +7,22 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function impresszum() {
-        $title = "Best-Recipes | Impresszum";
+        $title = "Impresszum";
         return view('pages.impresszum')->with('title', $title);
     }
 
     public function adatvedelem() {
-        $title = "Best-Recipes | Adatvédelem";
+        $title = "Adatvédelem";
         return view('pages.adatvedelem')->with('title', $title);
     }
 
     public function suti() {
-        $title = "Best-Recipes | Süti beállítások";
+        $title = "Süti beállítások";
         return view('pages.suti')->with('title', $title);
     }
 
     public function felhasznalasi_feltetelek() {
-        $title = "Best-Recipes | Felhasználási feltételek";
+        $title = "Felhasználási feltételek";
         return view('pages.felhasznalasif')->with('title', $title);
     }
 }
