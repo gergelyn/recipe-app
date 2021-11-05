3ad5da06b541c1f61ac4154789d2634b372b3bf1
gergelyn <preszthegas@gmail.com>
2021-11-05 11:27:21 +0100

    Basic front-end for edit and delete posts routes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,6 +7,12 @@
             <div class="flex flex-col bg-white shadow-md w-3/12 rounded-xl">
                 <h4 class="ml-6 font-bold text-lg"><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h4>
                 <p class="ml-6 my-2 text-xs">{{ $post->created_at }}</p>
+                <form action="{{ route('posts.destroy', $post->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <a href="{{ route('posts.edit', $post->id) }}" class="rounded-full bg-blue-600 text-white px-4 py-2">Edit</a>
+                    <button type="submit" class="rounded-full bg-red-600 text-white px-4 py-2">DELETE</button>
+                </form>
             </div>
         @endforeach
     @else
