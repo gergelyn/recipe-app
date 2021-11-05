9ca74c5892bd6d06b911224e92437e68e5524f89
gergelyn <preszthegas@gmail.com>
2021-11-05 12:05:50 +0100

    flex div added for index posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,20 +8,23 @@
             <p>{{ $message }}</p>
         </div>
     @endif
-    @if (count($posts) > 0)
-        @foreach ($posts as $post)
-            <div class="flex flex-col bg-white shadow-md w-3/12 rounded-xl">
-                <h4 class="ml-6 font-bold text-lg"><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h4>
-                <p class="ml-6 my-2 text-xs">{{ $post->created_at }}</p>
-                <form action="{{ route('posts.destroy', $post->id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <a href="{{ route('posts.edit', $post->id) }}" class="rounded-full bg-blue-600 text-white px-4 py-2">Edit</a>
-                    <button type="submit" class="rounded-full bg-red-600 text-white px-4 py-2">DELETE</button>
-                </form>
-            </div>
-        @endforeach
-    @else
-        <p>No posts found!</p>
-    @endif
+    <div class="flex mx-48 justify-between">
+        @if (count($posts) > 0)
+            @foreach ($posts as $post)
+                <div class="flex flex-col bg-white shadow-md w-3/12 rounded-xl">
+                    <h4 class="ml-6 font-bold text-lg"><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h4>
+                    <p class="ml-6 my-2 text-xs">{{ $post->created_at }}</p>
+                    <form action="{{ route('posts.destroy', $post->id) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <a href="{{ route('posts.edit', $post->id) }}" class="rounded-full bg-blue-600 text-white px-4 py-2">Edit</a>
+                        <button type="submit" class="rounded-full bg-red-600 text-white px-4 py-2">DELETE</button>
+                    </form>
+                </div>
+            @endforeach
+        @else
+            <p>No posts found!</p>
+        @endif
+    </div>
+    
 @endsection
