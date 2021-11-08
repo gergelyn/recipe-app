1cb5dcbfdef8ee2b9b03917e4bbffcddbd6722f8
gergelyn <preszthegas@gmail.com>
2021-11-08 09:24:18 +0100

    Basic front-end for showing Post's image

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <img src="{{ $image->post_image_path }}" alt="{{ $image->post_image_caption }}">
     <h1 class="text-lg font-bold">{{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
     <small>{{ $post->created_at }}</small>
