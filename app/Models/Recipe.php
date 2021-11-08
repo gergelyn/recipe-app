ee4793c1ee7ca344fc0ff77318f10d1299eb9c8b
gergelyn <preszthegas@gmail.com>
2021-11-08 11:04:03 +0100

    Relationship between Recipe and RecipeDifficulty

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+
+    public function difficulty() {
+        return $this->hasOne('App\RecipeDifficulty');
+    }
 }
