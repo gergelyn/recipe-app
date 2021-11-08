ce82193468b0c16317c3c099f160385e8effa5f7
gergelyn <preszthegas@gmail.com>
2021-11-08 11:04:54 +0100

    Create recipes table migration

---
+++ database/migrations/2021_11_08_110059_create_recipes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('cook_time');
+            $table->unsignedBigInteger('difficulty_id');
+            $table->foreign('difficulty_id')->references('id')->on('recipe_difficulties');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
