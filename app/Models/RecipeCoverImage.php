4d46d6b0d78203e7144a49c1a28e172b1a4a93a1
gergelyn <preszthegas@gmail.com>
2021-11-10 16:01:55 +0100

    RecipeCoverImage Model with Relationship

---
+++ app/Models/RecipeCoverImage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RecipeCoverImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'recipe_id',
+        'recipe_image_path',
+        'recipe_image_caption'
+    ];
+
+    public function recipe() {
+        return $this->belongsTo('App\Recipe');
+    }
+}
