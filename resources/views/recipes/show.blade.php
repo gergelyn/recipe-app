aec7aba28110c14000448e5e595e579affe9cd1f
gergelyn <preszthegas@gmail.com>
2021-11-10 16:41:24 +0100

    Show View updated with showing recipe's image

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <img src="{{ $recipe->recipe_image_path }}" alt="{{ $recipe->recipe_image_caption }}">
     <h1 class="text-lg font-bold">{{ $recipe->title }}</h1>
     <p>{{ $recipe->cook_time }}</p>
     <p>{{ $recipe->level }}</p>
