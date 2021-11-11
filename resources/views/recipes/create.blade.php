48f248689e77f0ce9a0ec1cbec066a330f5e79bf
gergelyn <preszthegas@gmail.com>
2021-11-11 10:04:31 +0100

    RecipeMealType select input

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -25,7 +25,15 @@
         <label for="difficulty_id">Nehézség</label>
         <select name="difficulty_id" id="difficulty_id">
             @foreach ($difficulties as $difficulty)
+                @dump($difficulty)
                 <option value="{{ $difficulty->id }}">{{ $difficulty->level }}</option>
+            @endforeach
+        </select>
+        <label for="meal_type_id">Típus</label>
+        @dump($meal_types)
+        <select name="meal_type_id" id="meal_type_id">
+            @foreach ($meal_types as $meal_type)
+                <option value="{{ $meal_type->id }}">{{ $meal_type->meal_type }}</option>
             @endforeach
         </select>
         <button type="submit" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700">Posztolás</button>
