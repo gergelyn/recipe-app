e659a8cfdf5dd090264f65d183bd73b14d10d8e4
gergelyn <preszthegas@gmail.com>
2021-11-11 10:04:38 +0100

    RecipeMealType select input

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -28,6 +28,12 @@
                 <option value="{{ $difficulty->id }}">{{ $difficulty->level }}</option>
             @endforeach
         </select>
+        <label for="meal_type_id">Típus</label>
+        <select name="meal_type_id" id="meal_type_id">
+            @foreach ($meal_types as $meal_type)
+                <option value="{{ $meal_type->id }}">{{ $meal_type->meal_type }}</option>
+            @endforeach
+        </select>
         <button type="submit" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700">Szerkesztés befejezése</button>
     </form>
 @endsection
