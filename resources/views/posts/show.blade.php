f9bd02df90ada23511c4490fd7a7d186a9032eab
gergelyn <preszthegas@gmail.com>
2021-11-14 22:40:35 +0100

    Show author's name

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,6 @@
     <img src="{{ $image->post_image_path }}" alt="{{ $image->post_image_caption }}">
     <h1 class="text-lg font-bold">{{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
+    <p>{{ $post->author->name }}</p>
     <small>{{ $post->created_at }}</small>
 @endsection
