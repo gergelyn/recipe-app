52591675d7e489466b3cf061a1ac71689a34b76d
gergelyn <preszthegas@gmail.com>
2021-11-14 10:41:35 +0100

    New way of showing Model properties

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <img src="{{ $recipe->recipe_image_path }}" alt="{{ $recipe->recipe_image_caption }}">
+    <img src="{{ $cover_image->recipe_image_path }}" alt="{{ $cover_image->recipe_image_caption }}">
     <h1 class="text-lg font-bold">{{ $recipe->title }}</h1>
     <p>{{ $recipe->cook_time }}</p>
-    <p>{{ $recipe->level }}</p>
-    <p>{{ $recipe->meal_type }}</p>
+    <p>{{ $difficulty }}</p>
+    <p>{{ $meal_type }}</p>
     <small>{{ $recipe->created_at }}</small>
 @endsection
