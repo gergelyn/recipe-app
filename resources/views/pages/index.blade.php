864a31f8bd72eed679fa144c88c9366b05b2cd9f
gergelyn <preszthegas@gmail.com>
2021-11-15 11:10:54 +0100

    Error handling

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="grid grid-cols-3 gap-4 mt-4">
+        @if ($soup === null)
+            <p>Nem található leves!</p>
+        @else
         <div class="flex flex-col bg-white shadow-md rounded-xl">
             <img src="{{ $soup->cover_image->recipe_image_path }}" alt="{{ $soup->cover_image->recipe_image_caption }}">
             <h4 class="ml-6 font-bold text-lg"><a href="/recipes/{{ $soup->id }}">{{ $soup->title }}</a></h4>
@@ -9,6 +12,10 @@
             <span class="ml-6 my-2 text-xs">{{ Str::title($difficulties->find($soup->difficulty_id)->level) }}</span>
             <span class="ml-6 my-2 text-xs">{{ Str::title($meal_types->find($soup->meal_type_id)->meal_type) }}</span>
         </div>
+        @endif
+        @if ($main_course === null)
+            <p>Nem található fôétel!</p>
+        @else
         <div class="flex flex-col bg-white shadow-md rounded-xl">
             <img src="{{ $main_course->cover_image->recipe_image_path }}" alt="{{ $main_course->cover_image->recipe_image_caption }}">
             <h4 class="ml-6 font-bold text-lg"><a href="/recipes/{{ $main_course->id }}">{{ $main_course->title }}</a></h4>
@@ -16,6 +23,10 @@
             <span class="ml-6 my-2 text-xs">{{ Str::title($difficulties->find($main_course->difficulty_id)->level) }}</span>
             <span class="ml-6 my-2 text-xs">{{ Str::title($meal_types->find($main_course->meal_type_id)->meal_type) }}</span>
         </div>
+        @endif
+        @if ($dessert === null)
+            <p>Nem található desszert!</p>
+        @else
         <div class="flex flex-col bg-white shadow-md rounded-xl">
             <img src="{{ $dessert->cover_image->recipe_image_path }}" alt="{{ $dessert->cover_image->recipe_image_caption }}">
             <h4 class="ml-6 font-bold text-lg"><a href="/recipes/{{ $dessert->id }}">{{ $dessert->title }}</a></h4>
@@ -23,5 +34,7 @@
             <span class="ml-6 my-2 text-xs">{{ Str::title($difficulties->find($dessert->difficulty_id)->level) }}</span>
             <span class="ml-6 my-2 text-xs">{{ Str::title($meal_types->find($dessert->meal_type_id)->meal_type) }}</span>
         </div>
+        @endif
+        
     </div>
 @endsection
