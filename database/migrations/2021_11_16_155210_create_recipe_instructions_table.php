fa9a4edd86c65ad9c4d166bb41f729cf38b21ae1
gergelyn <preszthegas@gmail.com>
2021-11-16 15:56:24 +0100

    migration for recipe_instructions table

---
+++ database/migrations/2021_11_16_155210_create_recipe_instructions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipeInstructionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipe_instructions', function (Blueprint $table) {
+            $table->id();
+            $table->text('instruction_text');
+            $table->foreignId('recipe_id')
+                ->constrained('recipes')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipe_instructions');
+    }
+}
