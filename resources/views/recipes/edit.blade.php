f54a5b494fea94f9653ed103e9ec08436edacd24
gergelyn <preszthegas@gmail.com>
2021-11-16 18:27:25 +0100

    Basic edit form for ingredients and instructions

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -13,27 +13,138 @@
             </ul>
         </div>
     @endif
-    <form action="{{ route('recipes.update', $recipe->id) }}" method="POST" class="flex flex-col w-1/3" enctype="multipart/form-data">
+    <form action="{{ route('recipes.update', $recipe->id) }}" method="POST" class="flex flex-col w-1/2" enctype="multipart/form-data">
         @csrf
         @method('PUT')
-        <label for="image">Recept képe</label>
-        <input type="file" name="image" id="image" accept="image/*">
-        <label for="title">Cím</label>
-        <input id="title" type="text" name="title" class="border-2 border-black rounded-md" value="{{ $recipe->title }}">
-        <label for="cook_time">Idô</label>
-        <input id="cook_time" type="number" name="cook_time" class="border-2 border-black rounded-md" value="{{ $recipe->cook_time }}">
-        <label for="difficulty_id">Nehézség</label>
-        <select name="difficulty_id" id="difficulty_id">
-            @foreach ($difficulties as $difficulty)
-                <option value="{{ $difficulty->id }}">{{ $difficulty->level }}</option>
+        
+        <div class="mb-4">
+            <label for="image">Recept képe</label>
+            <input type="file" name="image" id="image" accept="image/*">
+        </div>
+
+        <div class="mb-4">
+            <label for="title">Cím</label>
+            <input id="title" type="text" name="title" class="border-2 border-black rounded-md" value="{{ $recipe->title }}">
+        </div>
+        
+        <div class="mb-4">
+            <label for="cook_time">Idô</label>
+            <input id="cook_time" type="number" name="cook_time" class="border-2 border-black rounded-md" value="{{ $recipe->cook_time }}">
+        </div>
+
+        <div class="mb-4">
+            <label for="difficulty_id">Nehézség</label>
+            <select name="difficulty_id" id="difficulty_id">
+                @foreach ($difficulties as $difficulty)
+                    <option value="{{ $difficulty->id }}">{{ $difficulty->level }}</option>
+                @endforeach
+            </select>
+        </div>
+        
+
+        <div class="mb-4">
+            <label for="meal_type_id">Típus</label>
+            <select name="meal_type_id" id="meal_type_id">
+                @foreach ($meal_types as $meal_type)
+                    <option value="{{ $meal_type->id }}">{{ $meal_type->meal_type }}</option>
+                @endforeach
+            </select>
+        </div>
+        
+        <div class="mb-4 ingredients">
+            <label for="ingredients">Alapanyagok</label>
+            @foreach ($recipe->ingredients as $ingredient)
+            <div class="ingredient my-2">
+                <input type="number" name="measurement_amounts[]" id="measurement_amounts" class="border-2 border-black rounded-md" placeholder="Mennyiség" value={{ $ingredient->measurement_qty->amount }}>
+                <select name="unit_type_ids[]" id="unit_type_ids" class="border-2 border-black rounded-md">
+                    @foreach ($unit_types as $unit_type)
+                        <option value="{{ $unit_type->id }}">{{ $unit_type->unit_type }}</option>
+                    @endforeach
+                </select>
+                <input type="text" name="ingredient_names[]" id="ingredient_names" class="border-2 border-black rounded-md" placeholder="Alapanyag" value={{ $ingredient->ingredient_name->ingredient_name }}>
+            </div>
             @endforeach
-        </select>
-        <label for="meal_type_id">Típus</label>
-        <select name="meal_type_id" id="meal_type_id">
-            @foreach ($meal_types as $meal_type)
-                <option value="{{ $meal_type->id }}">{{ $meal_type->meal_type }}</option>
+        </div>
+
+        <div class="mb-4">
+            <a id="plus-ingredient" class="w-full border border-blue-600 p-3 cursor-pointer bg-blue-600 hover:border-blue-700 hover:bg-blue-700 text-white">+</a>
+        </div>
+
+        <div class="mb-4 instructions">
+            <label for="instructions">Elkészítés</label>
+            @foreach ($recipe->instructions()->get() as $instruction)
+            <div class="instruction my-2">
+                <input type="text" name="instructions[]" id="instruction" class="border-2 border-black rounded-md" placeholder="Teendô" value="{{ $instruction->instruction_text }}">
+            </div>
             @endforeach
-        </select>
+        </div>
+        
+        <div class="mb-4">
+            <a id="plus-instruction" class="w-full border border-blue-600 p-3 cursor-pointer bg-blue-600 hover:border-blue-700 hover:bg-blue-700 text-white">+</a>
+        </div>
+
         <button type="submit" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700">Szerkesztés befejezése</button>
     </form>
+@endsection
+
+@section('script')
+    <script>
+        let plusIngredientBtn = document.querySelector("#plus-ingredient");
+        let ingredientsContainer = document.querySelector(".ingredients");
+        
+        let plusInstructionBtn = document.querySelector("#plus-instruction");
+        let instructionsContainer = document.querySelector(".instructions");
+
+        plusIngredientBtn.addEventListener("click", function() {
+            let unitTypes = [
+                @foreach($unit_types as $unit_type)
+                    ["{{ $unit_type->id }}", "{{ $unit_type->unit_type }}"],
+                @endforeach
+            ];
+            let ingredientContainer = document.createElement("div");
+            ingredientContainer.setAttribute("class", "ingredient my-2")
+            let unitTypeIdSelect = document.createElement("select");
+            unitTypeIdSelect.setAttribute("id", "unit_type_ids");
+            unitTypeIdSelect.setAttribute("class", "border-2 border-black rounded-md");
+            unitTypeIdSelect.setAttribute("name", "unit_type_ids[]");
+
+            for(let i = 0; i<unitTypes.length; i++) {
+                let unitTypeOption = document.createElement("option");
+                unitTypeOption.setAttribute("value", unitTypes[i][0]);
+                unitTypeOption.innerText = unitTypes[i][1];
+                unitTypeIdSelect.appendChild(unitTypeOption);
+            }
+
+            let measurementAmountInput = document.createElement("input");
+            measurementAmountInput.setAttribute("type", "number");
+            measurementAmountInput.setAttribute("name", "measurement_amounts[]");
+            measurementAmountInput.setAttribute("class", "border-2 border-black rounded-md");
+            measurementAmountInput.setAttribute("id", "measurement_amounts");
+            measurementAmountInput.setAttribute("placeholder", "Mennyiség");
+
+            let ingredientNameInput = document.createElement("input");
+            ingredientNameInput.setAttribute("type", "text");
+            ingredientNameInput.setAttribute("name", "ingredient_names[]");
+            ingredientNameInput.setAttribute("class", "border-2 border-black rounded-md");
+            ingredientNameInput.setAttribute("id", "ingredient_names");
+            ingredientNameInput.setAttribute("placeholder", "Alapanyag");
+
+            ingredientContainer.appendChild(measurementAmountInput);
+            ingredientContainer.appendChild(unitTypeIdSelect);
+            ingredientContainer.appendChild(ingredientNameInput);
+            ingredientsContainer.appendChild(ingredientContainer);
+        });
+
+        plusInstructionBtn.addEventListener("click", function() {
+            let instructionContainer = document.createElement("div");
+            instructionContainer.setAttribute("class", "instruction my-2");
+            let instructionInput = document.createElement("input");
+            instructionInput.setAttribute("type", "text");
+            instructionInput.setAttribute("name", "instructions[]");
+            instructionInput.setAttribute("class", "border-2 border-black rounded-md");
+            instructionInput.setAttribute("placeholder", "Teendô");
+            instructionContainer.appendChild(instructionInput);
+            instructionsContainer.appendChild(instructionContainer);
+        });
+    </script>
 @endsection
