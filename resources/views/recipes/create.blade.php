86f94692f373c3f8622d56d6fcd0b2275d5c4f63
gergelyn <preszthegas@gmail.com>
2021-11-16 13:11:44 +0100

    Basic script for creating ingredient input-groups

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -23,12 +23,12 @@
         
         <div class="mb-4">
             <label for="title">Cím</label>
-            <input id="title" type="text" name="title" class="border-2 border-black rounded-md">
+            <input id="title" type="text" name="title" class="border-2 border-black rounded-md" value={{ old('title') }}>
         </div>
         
         <div class="mb-4">
             <label for="cook_time">Idô</label>
-            <input id="cook_time" type="number" name="cook_time" class="border-2 border-black rounded-md">
+            <input id="cook_time" type="number" name="cook_time" class="border-2 border-black rounded-md" value={{ old('cook_time') }}>
         </div>
 
         <div class="mb-4">
@@ -49,15 +49,70 @@
             </select>
         </div>
 
-        <div class="mb-4">
+        <div class="mb-4 ingredients">
             <label for="ingredients">Alapanyagok</label>
-            <select name="unit_type" id="unit_type" class="border-2 border-black rounded-md">
-                
-            </select>
-            <input type="number" name="measurement_amount" id="measurement_amount" class="border-2 border-black rounded-md" placeholder="Mennyiség">
-            <input type="text" name="ingredient_name" id="ingredient_name" class="border-2 border-black rounded-md" placeholder="Alapanyag">
+            <div class="ingredient my-2">
+                <select name="unit_type_ids[]" id="unit_type_ids" class="border-2 border-black rounded-md">
+                    @foreach ($unit_types as $unit_type)
+                        <option value="{{ $unit_type->id }}">{{ $unit_type->unit_type }}</option>
+                    @endforeach
+                </select>
+                <input type="number" name="measurement_amounts[]" id="measurement_amounts" class="border-2 border-black rounded-md" placeholder="Mennyiség">
+                <input type="text" name="ingredient_names[]" id="ingredient_names" class="border-2 border-black rounded-md" placeholder="Alapanyag">
+            </div>
+        </div>
+
+        <div class="mb-4">
+            <a id="plus-ingredient" class="w-full border border-blue-600 p-3 cursor-pointer bg-blue-600 hover:border-blue-700 hover:bg-blue-700 text-white">+</a>
         </div>
         
         <button type="submit" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700 mt-2">Posztolás</button>
     </form>
+@endsection
+
+@section('script')
+    <script>
+        let plusBtn = document.querySelector("#plus-ingredient");
+        let ingredientsContainer = document.querySelector(".ingredients");
+
+        plusBtn.addEventListener("click", function() {
+            let unitTypes = [
+                @foreach($unit_types as $unit_type)
+                    ["{{ $unit_type->id }}", "{{ $unit_type->unit_type }}"],
+                @endforeach
+            ];
+            let ingredientContainer = document.createElement("div");
+            ingredientContainer.setAttribute("class", "ingredient my-2")
+            let unitTypeIdSelect = document.createElement("select");
+            unitTypeIdSelect.setAttribute("id", "unit_type_ids");
+            unitTypeIdSelect.setAttribute("class", "border-2 border-black rounded-md");
+            unitTypeIdSelect.setAttribute("name", "unit_type_ids[]");
+
+            for(let i = 0; i<unitTypes.length; i++) {
+                let unitTypeOption = document.createElement("option");
+                unitTypeOption.setAttribute("value", unitTypes[i][0]);
+                unitTypeOption.innerText = unitTypes[i][1];
+                unitTypeIdSelect.appendChild(unitTypeOption);
+            }
+
+            let measurementAmountInput = document.createElement("input");
+            measurementAmountInput.setAttribute("type", "number");
+            measurementAmountInput.setAttribute("name", "measurement_amounts[]");
+            measurementAmountInput.setAttribute("class", "border-2 border-black rounded-md");
+            measurementAmountInput.setAttribute("id", "measurement_amounts");
+            measurementAmountInput.setAttribute("placeholder", "Mennyiség");
+
+            let ingredientNameInput = document.createElement("input");
+            ingredientNameInput.setAttribute("type", "text");
+            ingredientNameInput.setAttribute("name", "ingredient_names[]");
+            ingredientNameInput.setAttribute("class", "border-2 border-black rounded-md");
+            ingredientNameInput.setAttribute("id", "ingredient_names");
+            ingredientNameInput.setAttribute("placeholder", "Alapanyag");
+
+            ingredientContainer.appendChild(unitTypeIdSelect);
+            ingredientContainer.appendChild(measurementAmountInput);
+            ingredientContainer.appendChild(ingredientNameInput);
+            ingredientsContainer.appendChild(ingredientContainer);
+        });
+    </script>
 @endsection
