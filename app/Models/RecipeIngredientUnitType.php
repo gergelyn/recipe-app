4c70b8f3c445c635f2f1e1458f0eb53bb4dfab94
gergelyn <preszthegas@gmail.com>
2021-11-16 13:13:56 +0100

    RecipeIngredientUnitType model init

---
+++ app/Models/RecipeIngredientUnitType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RecipeIngredientUnitType extends Model
+{
+    use HasFactory;
+}
