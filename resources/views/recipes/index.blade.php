9bac8ae21911f966db143449d06e6bf306e3b7bf
Janos Geszteredi <geszteredi.janika98@gmail.com>
2021-12-04 20:15:53 +0100

    /recipes/index.blade.php formatting

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,25 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-lg font-bold mb-2">Receptek</h1>
+    <div class="flex flex-row">
+        <div class="flex items-center">
+            <form action="/search" method="GET">
+                <input type="search" name="query" id="query" class="border-2 border-yellow-600 rounded-full px-4 py-2" placeholder="Recept keresés">
+            </form>
+        </div>
+        <button class="bg-yellow-600 rounded-full px-4 py-2 text-white ml-2">KERESÉS</button>
+    </div>
     @auth
-    <a href="{{ route('recipes.create') }}" class="rounded-full bg-green-600 text-white px-4 py-2">Recept posztolás</a>
+    <div class="my-6">
+        <a href="{{ route('recipes.create') }}" class="rounded-full bg-green-600 text-white px-4 py-2">Recept posztolás</a>
+    </div>
     @endauth
     @if ($message = Session::get('status'))
         <div class="bg-green-600 text-white w-1/3">
             <p>{{ $message }}</p>
         </div>
     @endif
+
+
     <div class="grid grid-cols-3 gap-4 mt-4">
         @if ($recipes->count())
             @foreach ($recipes as $recipe)
                 <div class="flex flex-col bg-white shadow-md rounded-xl">
-                    <img src="{{ $recipe->cover_image->recipe_image_path }}" alt="{{ $recipe->cover_image->recipe_image_caption }}">
-                    <h4 class="ml-6 font-bold text-lg"><a href="/recipes/{{ $recipe->id }}">{{ $recipe->title }}</a></h4>
-                    <span class="ml-6 my-2 text-xs">{{ date("Y-m-d", strtotime($recipe->created_at)) }}</span>
-                    <span class="ml-6 my-2 text-xs">{{ Str::title($difficulties->find($recipe->difficulty_id)->level) }}</span>
-                    <span class="ml-6 my-2 text-xs">{{ Str::title($meal_types->find($recipe->meal_type_id)->meal_type) }}</span>
-                    <span class="ml-6 my-2 text-xs">{{ $recipe->author->name }}</span>
+                    <img style="height: 200px" src="{{ $recipe->cover_image->recipe_image_path }}" alt="{{ $recipe->cover_image->recipe_image_caption }}">
+                    <h4 class="ml-6 mt-4 font-bold text-lg"><a href="/recipes/{{ $recipe->id }}">{{ $recipe->title }}</a></h4>
+                    <span class="ml-6 my-2 text-xs">{{ $recipe->author->name }}-tól</span>
+                    <div class="mb-2 flex flex-row justify-between">
+                        <span class="ml-6 my-2 text-xs">{{ $recipe->cook_time }} perc</span>
+                        <span class="mr-16 my-2 text-xs">{{ Str::title($difficulties->find($recipe->difficulty_id)->level) }}</span>
+                    </div>
                     @auth
                     <form action="{{ route('recipes.destroy', $recipe->id) }}" method="POST">
                         @csrf
@@ -34,5 +46,5 @@
             <p>Nem található recept!</p>
         @endif
     </div>
-    
+
 @endsection
