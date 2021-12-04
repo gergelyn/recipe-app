d3d76c34d8633a00408dc575d9c4b1f6f361196f
Janos Geszteredi <geszteredi.janika98@gmail.com>
2021-12-04 20:18:20 +0100

    formatted show.blade.php for recipes

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,28 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <img src="{{ $cover_image->recipe_image_path }}" alt="{{ $cover_image->recipe_image_caption }}">
-    <h1 class="text-lg font-bold">{{ $recipe->title }}</h1>
-    <p>{{ $recipe->cook_time }}</p>
-    <p>{{ $difficulty }}</p>
-    <p>{{ $meal_type }}</p>
-    <h3 class="text-md font-bold">Alapanyagok</h3>
-    <form action="#"></form>
-        @foreach ($recipe->ingredients as $ingredient)
-        <li>
-            <span>{{ $ingredient->measurement_qty->amount }}</span>
-            <span>{{ $ingredient->measurement_unit->unit_type }}</span>
-            <span>{{ $ingredient->ingredient_name->ingredient_name }}</span>
-        </li>
-        @endforeach
-    </form>
-    <h3 class="text-md font-bold">Elkészítés</h3>
-    <ol class="list-decimal">
-        @foreach ($instructions as $instruction)
-            <li class="ml-4">{{ $instruction->instruction_text }}</li>
-        @endforeach
-    </ol>
+    <div class="mx-auto w-1/2">
 
-    <small>{{ $recipe->author->name }}</small>
-    <small>{{ $recipe->created_at }}</small>
+        <img src="{{ $cover_image->recipe_image_path }}" alt="{{ $cover_image->recipe_image_caption }}" class="mb-6">
+        <h1 class="text-lg font-bold mb-4">{{ $recipe->title }}</h1>
+
+        <div class="flex flex-row">
+            <p>{{ $recipe->cook_time }} perc</p>
+            <p>{{ $difficulty }}</p>
+            <p>{{ $meal_type }}</p>
+        </div>
+
+        <hr class="my-4">
+
+        <h3 class="text-md font-bold">Hozzávalók</h3>
+        <form action="#"></form>
+            @foreach ($recipe->ingredients as $ingredient)
+            <li>
+                <span>{{ $ingredient->measurement_qty->amount }}</span>
+                <span>{{ $ingredient->measurement_unit->unit_type }}</span>
+                <span>{{ $ingredient->ingredient_name->ingredient_name }}</span>
+            </li>
+            @endforeach
+        </form>
+
+        <hr class="my-4">
+
+        <h3 class="text-md font-bold">Elkészítés</h3>
+        <ol class="list-decimal">
+            @foreach ($instructions as $instruction)
+                <li class="ml-4">{{ $instruction->instruction_text }}</li>
+            @endforeach
+        </ol>
+
+        <hr class="my-4">
+
+        <small>Recept készítője: {{ $recipe->author->name }}</small>
+    </div>
 @endsection
