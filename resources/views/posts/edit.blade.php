00f92b80e6480a87483eb3e5f08198868bffc4eb
gergelyn <preszthegas@gmail.com>
2021-12-05 14:05:46 +0100

    Integrate CKEditor 4

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -21,7 +21,17 @@
         <label for="title">Cím</label>
         <input id="title" type="text" name="title" class="border-2 border-black rounded-md" value="{{ $post->title }}">
         <label for="body">Szöveg</label>
-        <textarea id="body" name="body" class="border-2 border-black rounded-md" rows="5">{{ $post->body }}</textarea>
+        <textarea id="body" name="body" class="ckeditor border-2 border-black rounded-md" rows="5">{{ $post->body }}</textarea>
         <button type="submit" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700">Szerkesztés befejezése</button>
     </form>
+@endsection
+
+@section('script')
+    <script src="//cdn.ckeditor.com/4.17.1/standard/ckeditor.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            document.querySelector(".ckeditor").ckeditor();
+        });
+        
+    </script>
 @endsection
