91c6b3d26de721331b1d2f041c8b0d08b00b02ac
gergelyn <preszthegas@gmail.com>
2021-12-05 14:06:22 +0100

    Show rendered HTML by CKEditor

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <img src="{{ $image->post_image_path }}" alt="{{ $image->post_image_caption }}">
     <h1 class="text-lg font-bold">{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
+    <p>{!! $post->body !!}</p>
     <p>{{ $post->author->name }}</p>
     <small>{{ $post->created_at }}</small>
 @endsection
