1a1873827f212cad819b8262372294436fd2df57
Janos Geszteredi <geszteredi.janika98@gmail.com>
2021-12-05 22:44:45 +0100

    formatted posts/create.blade.php and posts/edit.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-lg font-bold">Blog posztolása</h1>
-    <a href="{{ route('posts.index') }}" class="rounded-full bg-blue-600 text-white px-4 py-2">Vissza</a>
+    <div class="mx-auto w-1/2">
+    <h1 class="text-lg font-bold mb-6 text-yellow-600">Blog posztolása</h1>
+{{--    <a href="{{ route('posts.index') }}" class="rounded-full bg-blue-600 text-white px-4 py-2">Vissza</a>--}}
     @if ($errors->any())
         <div class="bg-red-600 text-white w-1/3">
             <strong>Error!</strong>
@@ -14,16 +15,24 @@
         </div>
     @endif
 
-    <form action="{{ route('posts.store') }}" method="POST" class="flex flex-col w-1/3 mt-2" enctype="multipart/form-data">
+    <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <label for="image">Poszt képe</label>
-        <input id="image" type="file" name="image" accept="image/*">
-        <label for="title">Cím</label>
-        <input id="title" type="text" name="title" class="border-2 border-black rounded-md">
-        <label for="body">Szöveg</label>
-        <textarea id="body" name="body" class="ckeditor border-2 border-black rounded-md" rows="5"></textarea>
-        <button type="submit" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700 mt-2">Posztolás</button>
+        <div class="mb-4">
+            <label for="image">Poszt képe</label>
+            <input id="image" type="file" name="image" accept="image/*">
+        </div>
+        <div class="mb-4">
+            <label for="title">Cím</label>
+            <input id="title" type="text" name="title" class="border-2 border-black rounded-md">
+        </div>
+        <div class="mb-4">
+            <label for="body">Szöveg</label>
+            <textarea id="body" name="body" class="ckeditor border-2 border-black rounded-md" rows="5"></textarea>
+        </div>
+
+        <button type="submit" class="m-auto p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700 mt-2">Posztolás</button>
     </form>
+    </div>
 @endsection
 
 @section('script')
@@ -32,6 +41,6 @@
         document.addEventListener("DOMContentLoaded", function() {
             document.querySelector(".ckeditor").ckeditor();
         });
-        
+
     </script>
 @endsection
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-lg font-bold">Poszt szerkesztése</h1>
-    <a href="{{ route('posts.index') }}" class="rounded-full bg-blue-600 text-white px-4 py-2">Vissza</a>
+    <div class="mx-auto w-1/2">
+    <h1 class="text-lg font-bold mb-6 text-yellow-600">Poszt szerkesztése</h1>
+{{--    <a href="{{ route('posts.index') }}" class="rounded-full bg-blue-600 text-white px-4 py-2">Vissza</a>--}}
     @if ($errors->any())
         <div class="bg-red-600 text-white w-1/3">
             <strong>Error!</strong>
@@ -13,17 +14,25 @@
             </ul>
         </div>
     @endif
-    <form action="{{ route('posts.update', $post->id) }}" method="POST" class="flex flex-col w-1/3" enctype="multipart/form-data">
+    <form action="{{ route('posts.update', $post->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
-        <label for="image">Poszt képe</label>
-        <input id="image" type="file" name="image" accept="image/*">
-        <label for="title">Cím</label>
-        <input id="title" type="text" name="title" class="border-2 border-black rounded-md" value="{{ $post->title }}">
-        <label for="body">Szöveg</label>
-        <textarea id="body" name="body" class="ckeditor border-2 border-black rounded-md" rows="5">{{ $post->body }}</textarea>
-        <button type="submit" class="p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700">Szerkesztés befejezése</button>
+        <div class="mb-4">
+            <label for="image">Poszt képe</label>
+            <input id="image" type="file" name="image" accept="image/*">
+        </div>
+        <div class="mb-4">
+            <label for="title">Cím</label>
+            <input id="title" type="text" name="title" class="border-2 border-black rounded-md" value="{{ $post->title }}">
+        </div>
+        <div class="mb-4">
+            <label for="body">Szöveg</label>
+            <textarea id="body" name="body" class="ckeditor border-2 border-black rounded-md" rows="5">{{ $post->body }}</textarea>
+        </div>
+
+        <button type="submit" class="m-auto p-3 border border-yellow-600 bg-yellow-600 rounded-full text-white hover:border-yellow-700 hover:bg-yellow-700">Szerkesztés befejezése</button>
     </form>
+    </div>
 @endsection
 
 @section('script')
@@ -32,6 +41,6 @@
         document.addEventListener("DOMContentLoaded", function() {
             document.querySelector(".ckeditor").ckeditor();
         });
-        
+
     </script>
 @endsection
