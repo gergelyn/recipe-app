a2abd380dd6afcdfd01d50ddf3c265c65a48f867
Janos Geszteredi <geszteredi.janika98@gmail.com>
2021-12-06 10:22:02 +0100

    fixed height for blog images

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
     @auth
     <a href="{{ route('posts.create') }}" class="rounded-full bg-green-600 text-white px-4 py-2">Poszt kiírása</a>
     @endauth
-    
+
     @if ($message = Session::get('status'))
         <div class="bg-green-600 text-white w-1/3">
             <p>{{ $message }}</p>
@@ -15,7 +15,7 @@
         @if (count($posts) > 0)
             @foreach ($posts as $post)
                 <div class="flex flex-col bg-white shadow-md rounded-xl">
-                    <img src="{{ $post->cover_image->post_image_path }}" alt="{{ $post->cover_image->post_image_caption }}">
+                    <img style="height: 200px" src="{{ $post->cover_image->post_image_path }}" alt="{{ $post->cover_image->post_image_caption }}">
                     <h4 class="ml-6 font-bold text-lg"><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h4>
                     <p class="ml-6 my-2 text-xs">{{ $post->created_at }}</p>
                     <p class="ml-6 my-2 text-xs">{{ $post->author->name }}</p>
@@ -33,5 +33,5 @@
             <p>Nem található poszt!</p>
         @endif
     </div>
-    
+
 @endsection
